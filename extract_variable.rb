30677a7e080f00fe58fc127988d284f428a0bcd0
gat41 <gloria.tengaihui@gmail.com>
2017-01-09 21:24:53 +0800

    add extract_variable challenge

---
+++ extract_variable.rb
@@ -0,0 +1,12 @@
+# extract_variable.rb
+#https://refactoring.com/catalog/extractVariable.html
+
+def win_prize?(username)
+
+	if ( username.length > 3 && username.length <= 7 ) || ( username.downcase.start_with?("a, e, i, o, u") )
+		puts "Congratulations #{username}! You won 1 million dollars!"
+	else
+		puts "Please try again!"
+	end
+
+end
