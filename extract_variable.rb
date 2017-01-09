04ba23aee6b14dc22a25d1a58cacfabc00f78717
gat41 <gloria.tengaihui@gmail.com>
2017-01-09 21:25:32 +0800

    remove method def in extract_variable

--- extract_variable.rb
+++ extract_variable.rb
@@ -1,12 +1,11 @@
 # extract_variable.rb
 #https://refactoring.com/catalog/extractVariable.html
 
-def win_prize?(username)
+username = "Alice"
 
-	if ( username.length > 3 && username.length <= 7 ) || ( username.downcase.start_with?("a, e, i, o, u") )
-		puts "Congratulations #{username}! You won 1 million dollars!"
-	else
-		puts "Please try again!"
-	end
-
+if ( username.length > 3 && username.length <= 7 ) || ( username.downcase.start_with?("a, e, i, o, u") )
+	puts "Congratulations #{username}! You won 1 million dollars!"
+else
+	puts "Please try again!"
 end
+
