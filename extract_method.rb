c2c8c8ab785ffe592543a5a09a7bdc4378355dbf
Wee Jing Heng <wheng3@illinois.edu>
2017-10-02 14:09:51 +0800

    extract_method completed

--- extract_method.rb
+++ extract_method.rb
@@ -1,9 +1,12 @@
 #extract_method.rb
 #use this as tips for the first one : http://refactoring.com/catalog/extractMethod.html
+def valid_length_and_start_with?(username, *args)
+	return ( username.length > 3 && username.length <= 10 ) || ( username.downcase.start_with?(*args) )
+end
 
 username = "Alice"
 
-if ( username.length > 3 && username.length <= 10 ) || ( username.downcase.start_with?("a", "e", "i", "o", "u") )
+if valid_length_and_start_with?(username, ["a", "e", "i", "o", "u"])
 	puts "Congratulations #{username}! You won 10 dollars!"
 else
 	puts "Thanks for joining!"
@@ -11,7 +14,7 @@ end
 
 username = "Bobby"
 
-if ( username.length > 3 && username.length <= 10 ) || ( username.downcase.start_with?("b", "g", "l", "p") )
+if (valid_length_and_start_with?(username, ["b", "g", "l", "p"]))
 	puts "Congratulations #{username}! You won 50 dollars!"
 else
 	puts "Thanks for joining!"
@@ -19,7 +22,7 @@ end
 
 username = "Xena"
 
-if ( username.length > 3 && username.length <= 10 ) || ( username.downcase.start_with?("q", "u", "x", "y", "z") )
+if (valid_length_and_start_with?(username, ["q", "u", "x", "y", "z"]))
 	puts "Congratulations #{username}! You won 100 dollars!"
 else
 	puts "Thanks for joining!"
